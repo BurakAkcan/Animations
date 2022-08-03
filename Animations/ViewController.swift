6231915796aeaa81dabb6af258d7ceae29279739
Burak AKCAN <106486885+BurakAkcan@users.noreply.github.com>
2022-08-03 11:18:27 +0300

    Initial Commit

---
+++ Animations/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Animations
+//
+//  Created by Burak AKCAN on 3.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
