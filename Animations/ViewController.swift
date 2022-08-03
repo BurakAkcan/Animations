76bf8a5e872031aa9e7810cccefb5cbf0b0ead18
Burak AKCAN <106486885+BurakAkcan@users.noreply.github.com>
2022-08-03 14:02:30 +0300

    Basic animations

--- Animations/ViewController.swift
+++ Animations/ViewController.swift
@@ -9,11 +9,58 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myView: UIView!
+    @IBOutlet weak var myViewTopConstrait: NSLayoutConstraint!
+    var isActive = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        print(myView.frame.origin.y)
+       hideAlert()
     }
+    @IBAction func buttonClick(_ sender: UIButton) {
+       springAnimation(show: isActive)
+    }
+    
+    private func animateShow(_ show:Bool){
+        
+        UIView.animate(withDuration: 1) {
+            if show {
+                self.hideAlert()
+            }else{
+                self.showAlert()
+            }
+            self.view.layoutIfNeeded()
+        }
+    }
+    
+    private func springAnimation(show:Bool){
+        UIView.animate(withDuration: 1,
+                       delay:0 ,
+                       usingSpringWithDamping: 0.2,
+                       initialSpringVelocity: 0,
+                       options: [.curveEaseInOut]) { [weak self] in
+            if show {
+                self?.hideAlert()
+            }else{
+                self?.showAlert()
+            }
+            self?.view.layoutIfNeeded()
+        } completion: { _ in
+            print("animation completed")
+        }
 
+    }
+    
+    private func showAlert(){
+        isActive = true
+        myViewTopConstrait.constant = 20
+    }
+    
+    private func hideAlert(){
+        isActive = false
+        myViewTopConstrait.constant = -(myView.frame.origin.y) - myView.frame.height
+    }
 
 }
 
